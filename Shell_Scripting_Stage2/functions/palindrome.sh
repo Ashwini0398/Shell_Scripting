2ce8bb5968a755892539a43bbc08764d7cd8bc28
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-11 08:08:46 +0530

    ADDED: Palindrom using function

--- Shell_Scripting_Stage2/functions/palindrome.sh
+++ Shell_Scripting_Stage2/functions/palindrome.sh
@@ -1,22 +1,33 @@
 #!/bin/bash -x
 
+# show the palindrome
+
+
+
 function palindrome()
 {
 val1=$number1
 val=0
-while (( $val1 != 0  ))
+while [[ $val1 != 0  ]]
 do
 	val2=$(( $val1 % 10 ))
 	val=$(( $val * 10 + $val2 ))
 	val1=$(( $val1 / 10 ))
 done
-if (( $val == $number1 ))
+	val $(($number1))
+}
+function val()
+{
+if [[ $val == $number1 ]]
 then
-	echo " $val is palindrome "
+        echo " $val is palindrome "
+	
 else
-	echo " $val is not palindrome "
+        echo " $val is not palindrome "
+	
 fi
 }
-	read -p "Enter the Number " number1
-	result="$( palindrome $number1 )"
-	echo "$result"
+
+read -p "Enter the Number " number1
+result=$( palindrome $number1 )	
+echo $result
